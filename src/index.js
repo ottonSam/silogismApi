8694e813ab85cabce86e12b371db675c6b237aca
Otton Maia <thelifeisbad.samuel@gmail.com>
2020-02-16 17:37:11 -0300

    add cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes.js');
+const cors = require('cors');
 
 const app = express();
 
@@ -16,5 +17,6 @@ mongoose.connect(process.env.MONGO_URL,{
 
 app.use(express.json());
 app.use(routes);
+app.use(cors());
 
 app.listen(process.env.PORT || 3001);
