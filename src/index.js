ea04d32b54b94463f4b96e67b0ec3bdfdb7e0928
Otton Maia <thelifeisbad.samuel@gmail.com>
2020-02-20 02:31:02 -0300

    add config cors

--- src/index.js
+++ src/index.js
@@ -17,6 +17,10 @@ mongoose.connect(process.env.MONGO_URL,{
 
 app.use(express.json());
 app.use(routes);
-app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  app.use(cors());
+  next();
+});
 
 app.listen(process.env.PORT || 3001);
